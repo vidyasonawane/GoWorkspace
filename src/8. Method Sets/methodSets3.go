948c721c834128e6a2bc2045330a94f63e4edaac
vidya <vidyasonawane20@gmail.com>
2019-10-07 12:43:24 +0530

    8. Method Sets

---
+++ src/8. Method Sets/methodSets3.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+import "math"
+
+//pointer receiver and pointer value
+type circle struct {
+	radius float64
+}
+
+type shape interface {
+	area() float64
+}
+
+func (c *circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func info(s shape) {
+	fmt.Println("area: ", s.area())
+}
+
+func main() {
+	c := circle{5}
+	info(&c)
+}
