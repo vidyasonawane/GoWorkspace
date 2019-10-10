93d6f31e869fd9d6ed256c6d7ff2aa02f67bcd57
vidya <vidyasonawane20@gmail.com>
2019-10-10 21:48:27 +0530

    Concurrency

---
+++ src/10. Concurrency/mutex.go
@@ -0,0 +1,40 @@
+package main
+
+//Mutex are used to remove the race condition
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	fmt.Println("CPUs: ", runtime.NumCPU())
+	fmt.Println("Goroutines: ", runtime.NumGoroutine())
+
+	counter := 0
+
+	const gs = 100
+	var wg sync.WaitGroup
+	wg.Add(gs)
+
+	var mu sync.Mutex
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			mu.Lock()
+			v := counter
+			runtime.Gosched()
+			v++
+			counter = v
+			mu.Unlock()
+			wg.Done()
+		}()
+		fmt.Println("Goroutines = ", runtime.NumGoroutine())
+
+	}
+	wg.Wait()
+	fmt.Println("Goroutines: ", runtime.NumGoroutine())
+	fmt.Println("count:", counter)
+}
+
+
---
+++ src/10. Concurrency/raceCondition.go
@@ -0,0 +1,36 @@
+package main
+//Run this using terminal and notepad
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	fmt.Println("CPUs: ", runtime.NumCPU())
+	fmt.Println("Goroutines: ", runtime.NumGoroutine())
+
+	counter := 0
+
+	const gs = 100
+	var wg sync.WaitGroup
+	wg.Add(gs)
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			v := counter
+			runtime.Gosched()
+			v++
+			counter = v
+			wg.Done()
+		}()
+		fmt.Println("Goroutines = ", runtime.NumGoroutine())
+
+	}
+	wg.Wait()
+	fmt.Println("Goroutines: ", runtime.NumGoroutine())
+	fmt.Println("count:", counter)
+}
+
+//To detect the race condition, run the following command
+//go run -race racecondition.go
