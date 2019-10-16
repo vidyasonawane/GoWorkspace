bc888572b770c1c40e860376b16e158a8930ba3d
vidya <vidyasonawane20@gmail.com>
2019-10-16 20:31:06 +0530

    Packages

---
+++ src/Packages/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"Packages/stringutil" //you can also specify the github link
+	"fmt"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!og ,olleH"))
+	fmt.Println(stringutil.MyName)
+}
